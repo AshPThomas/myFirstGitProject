a42594f6530cae9ab662064e0870442a8cea0f95
Ash Thomas <Ash.Thomas@me.com>
2017-12-07 17:15:49 +0100

    Initial Commit

---
+++ gitProject3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitProject3
+//
+//  Created by Ash Thomas on 07/12/2017.
+//  Copyright © 2017 Ash Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ gitProject3Tests/gitProject3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  gitProject3Tests.swift
+//  gitProject3Tests
+//
+//  Created by Ash Thomas on 07/12/2017.
+//  Copyright © 2017 Ash Thomas. All rights reserved.
+//
+
+import XCTest
+@testable import gitProject3
+
+class gitProject3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
